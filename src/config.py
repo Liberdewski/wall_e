d47d0b4789281dc926172bfdc050f03c9475eee1
Jason <jason_wang94@hotmail.com>
2018-07-12 23:56:32 -0700

    Initial commit of refactor

---
+++ src/config.py
@@ -0,0 +1,2 @@
+token = '<YOUR_BOT_TOKEN_HERE>'
+prefix = '.'
---
+++ src/wall_e.py
@@ -0,0 +1,39 @@
+# Module
+import config
+
+# Packages
+import asyncio
+import discord
+from discord.ext import commands
+
+
+########################################################
+# VARS
+########################################################
+bot = commands.Bot(command_prefix=config.prefix)
+
+########################################################
+# BOT EVENTS
+########################################################
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+########################################################
+# BOT COMMANDS
+########################################################
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('```pong!```')
+
+
+########################################################
+# RUN THE BOT
+########################################################
+bot.run(config.token)
